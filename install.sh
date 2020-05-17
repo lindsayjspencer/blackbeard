0f2ccf0ac214fc2c285abbf69212b7eda8d6f2e0
Lindsay Spencer <lindsayjspencer@gmail.com>
2020-05-17 20:21:52 +1000

    Moved service into repository

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo /etc/init.d/transmission-daemon stop
 echo "> Daemon service stopped."
 echo ""
 echo "// Insert transmission settings file"
-sudo cp ~/Scripts/startup/transmission.settings.json /etc/transmission-daemon/settings.json
+sudo cp lib/transmission.settings.json /etc/transmission-daemon/settings.json
 echo "> Saved."
 sudo /etc/init.d/transmission-daemon start
 echo "> Daemon service restarted."
